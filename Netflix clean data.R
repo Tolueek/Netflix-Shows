f75d47a12d134b95f7795742c230f01182d504f8
Tolueek <ogunmoyelatoluwalope@yahoo.com>
2024-12-03 23:59:59 -0700

    Netflix data analysis

---
+++ Netflix clean data.R
@@ -0,0 +1,13 @@
+
+install.packages("ggplot2")
+
+data <- read.csv('cleaned_netflix_data.csv')
+
+getwd()
+
+data <- read.csv("~/Nexford Assignment /Nexflix")
+data <- read.csv("C:/Nexford Assignment /Nexflix")
+data <- read.csv('cleaned_netflix_data.csv')
+data <- read.csv(file.choose())
+
+
